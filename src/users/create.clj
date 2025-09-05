7d19e76c2cdefa7bd8363c7d0eba827f0504685d
GustavoMaiaOliveira <gustavomaia334@gmail.com>
2025-09-05 15:23:06 -0300

    endpoint for create user is working

--- src/users/create.clj
+++ src/users/create.clj
@@ -1,30 +1,25 @@
 (ns users.create
-  (:require [cheshire.core :as json]))
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io]))
 
-;(def file-path "/home/gustavo_maia/IdeaProjects/api-crud-clojure/users.json")
-;
-;(defn create-users [request]
-;  (let [body (slurp (:body request))
-;        new-user (json/parse-string body true)
-;        existing-users (if (.exists (clojure.java.io/file file-path))
-;                         (json/parse-string (slurp file-path) true)
-;                         [])
-;        updated-users (conj existing-users new-user)]
-;    (spit file-path (json/generate-string updated-users {:pretty true}))
-;    {:status 201
-;     :headers {"Content-Type" "application/json"}
-;     :body (json/generate-string {:message "Usuário criado com sucesso!"})}))
+(def file-path "/home/gustavo_maia/IdeaProjects/api-crud-clojure/users.json")
 
 (defn create-users [request]
-  (let [user (:json-params request)]
-    (spit "/home/gustavo_maia/IdeaProjects/api-crud-clojure/users.json" (json/generate-string user {:pretty true}))
+  (let [new-user (:json-params request)
+    existing-users (if (.exists (io/file file-path))
+                     (json/parse-string (slurp file-path) true)
+                     [])
+    updated-users (conj existing-users new-user)]
+    (spit file-path
+          (str (json/generate-string new-user) "\n")
+          :append true)
     {:status 201
      :headers {"Content-Type" "application/json"}
      :body (json/generate-string {:message "Usuário criado com sucesso!"
-                                  :user user})}))
+                                  :user new-user})}))
 
 ;TODO
 ; - 1 criar endpoint post - DO
-; - Receber dados
-; - entender como salvar esses dados em um json ja existente
+; - Receber dados - DO
+; - entender como salvar esses dados em um json ja existente - DO
 ; - retornar info do user criado - DO
--- src/users/read.clj
+++ src/users/read.clj
@@ -12,35 +12,3 @@
   [request]
   {:status 200
    :body (get-users "/home/gustavo_maia/IdeaProjects/api-crud-clojure/users.json")})
-
-;(defn get-users
-;  [request]
-;  (println request)
-;  {:status 200
-;   :headers {"Content-Type" "application/json"}
-;   :body (get-users-in-off["/home/gustavo_maia/IdeaProjects/api-crud-clojure/users.json"])})
-
-;(defn read-users
-;  [file-path]
-;  {:status 200
-;   :headers {"Context-Type" "text/plain"}
-;   :body (get-users[file-path])})
-;
-;(read-users "/home/gustavo_maia/IdeaProjects/api-crud-clojure/users.json")
-
-;(defn funcao-hello
-;  [request]
-;  (print request)
-;  {:status 200
-;   :headers {"Content-Type" "text/plain"}
-;   :body "Hello World"})
-
-;(defn get-users
-;  [request]
-;  (println request)
-;  {:status 200
-;   :headers {"Content-Type" "application/json"}
-;   :body {:username "Gustavo Maia"
-;          :email "gustavo.maia@mmti.io"
-;          :country "Brazil"
-;          :city "SJP - PR"}})
